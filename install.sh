5c4aa50beacb8fdf9386780e7918febd2ab1ddc8
Sandeep A G <sarcot@jaguarlandrover.com.com>
2014-04-10 06:30:13 -0700

    Install file

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+clear
+cp -r ./wrt_plugin_src/Emailplugin /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/src/
+cp ./wrt_plugin_src/wrt-plugins-tizen-emailplugin.pc /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/pkgconfigs/
+cp ./wrt_plugin_src/wrt-plugins-tizen-emailplugin.pc.in /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/pkgconfigs/
+cp ./wrt_plugin_src/src_CMakeLists.txt /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/src/
+rm /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/src/CMakeLists.txt
+mv /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/src/src_CMakeLists.txt /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/src/CMakeLists.txt
+cd /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/ 
+cmake .
+cd src/Emailplugin
+make clean
+make
+cd ../..
+make install
+cp -rv /usr/local/lib/wrt-plugins/tizen-emailplugin /usr/lib/wrt-plugins/
+wrt-installer -p
+cd src/Emailplugin
+

30ccae1b8df3676c22d86b3a4588214fa2f05d20
ivipocjlr <gubaradk@jaguarlandrover.com>
2014-04-15 02:06:55 -0700

    email-services name updated

--- prep.sh
+++ prep.sh
@@ -4,7 +4,7 @@ zypper install ss-server
 zypper install msg-service
 zypper install msg-service-tools
 zypper install contacts-service2
-zypper install email-services 
+zypper install email-service 
 cp -r ./wrt_plugin_src/Emailplugin /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/src/
 cp ./wrt_plugin_src/wrt-plugins-tizen-emailplugin.pc.in /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/pkgconfigs/
 cd /prj/wrt-plugins-ivi/wrt-plugins-ivi-0.13.0/src 
